5d32b986dcae7b7af6159b5838701440e6660495
Artem <artemius2014poltava@gmail.com>
2020-04-26 20:28:56 +0300

    Add timer

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 window.addEventListener('DOMContentLoaded', function() {
 
 'use strict';
+
+// tabs
+
 let tab = document.querySelectorAll('.info-header-tab'),
     info = document.querySelector('.info-header'),
     tabContent = document.querySelectorAll('.info-tabcontent');
+
 
 function hideTabContent(a) {
     for (let i = a; i < tabContent.length; i++) {
@@ -33,4 +37,55 @@ info.addEventListener('click', function(event) {
         }
     }
 });
+
+// Timer
+
+let deadLine = '2018-05-21';
+
+function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+        // hours = Math.floor((t/1000/60/60) % 24);
+        // days = Math.floor((t/(1000*60*60*24)));
+
+    return {
+        'total' : t,
+        'hours' : hours,
+        'minutes' : minutes,
+        'seconds' : seconds
+    };
+}
+
+function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+        let t = getTimeRemaining(endtime);
+
+            function endZerro(num) {
+                if (num <= 9) {
+                    return '0' + num;
+                } else return num;
+            };
+
+        hours.textContent = endZerro(t.hours);
+        minutes.textContent = endZerro(t.minutes);
+        seconds.textContent = endZerro(t.seconds);
+
+        if (t.total <= 9 ) {
+            clearInterval(timeInterval);
+            hours.textContent = '00';
+            minutes.textContent = '00';
+            seconds.textContent = '00';
+        }
+    }
+}
+
+setClock('timer', deadLine);
 });
